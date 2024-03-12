9732c7748e629b4ba8d7eae0d6d277017a4d844c
paddak <paddak96@gmail.com>
2024-03-12 23:08:04 +0900

    Update LinearRegression.py

--- DeepLearning/LinearRegression.py
+++ DeepLearning/LinearRegression.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 #linear regression : y=ax+b
 #y(라벨,정답), x(피쳐,학습)을 이용하여 가장 적절한 a(가중치)와 b(편향)을 찾는과정
@@ -14,8 +14,8 @@ import matplotlib as plt
 #7. train
 
 #data
-x=np.array([1,2,3])
-y=np.array([4,5,6])
+X=np.array([np.random.randint(),np.random.randint(),np.random.randint()])
+Y=np.array([4,5,6])
 
 #weight, bias 초기화
 #local minimum방지, 다양한 초기조건에서 학습 시작->다양한 경로 탐색
@@ -28,6 +28,38 @@ def linear_regression(x):
 
 #loss function(MSE)
 def MSE(y_pred,y_true):
-    return tf.reduce_mean(tf.squre(y_pred-y_true))
+    return tf.reduce_mean(tf.square(y_pred-y_true))
 
+#optimizer 정의
+optimizer=tf.optimizers.SGD(learning_rate=0.01)
 
+#train function 정의
+def train_function(x,y):
+    #gradient기록
+    with tf.GradientTape() as tape:
+        y_pred=linear_regression(x)
+        loss=MSE(y_pred,y)
+    
+    gradients = tape.gradient(loss, [W,b])
+    optimizer.apply_gradients(zip(gradients, [W,b]))
+
+#train
+epochs=1000
+for epoch in range(epochs):
+    train_function(X,Y)
+    if epoch % 100==0:
+        print(f"Epoch {epoch}, Loss:{MSE(linear_regression(X),Y).numpy()}")
+
+# 학습된 가중치 및 편향 출력
+print("학습된 가중치:", W.numpy())
+print("학습된 편향:", b.numpy())
+
+# 시각화
+plt.scatter(X, Y, label='Data')
+plt.plot(X, linear_regression(X), color='red', label='Linear Regression')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Linear Regression')
+plt.legend()
+plt.grid(True)
+plt.show()
